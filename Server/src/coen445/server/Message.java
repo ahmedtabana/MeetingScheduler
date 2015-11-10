1a743fa7320a000ce85de5f793c7dfdd87d5ae0c
ahmedtabana <ahmedibraheem10@gmail.com>
2015-11-09 19:42:04 -0500

    Moved Message class from Server.jave to Message.java

---
+++ Server/src/coen445/server/Message.java
@@ -0,0 +1,43 @@
+package coen445.server;
+
+import java.net.DatagramPacket;
+import java.net.InetAddress;
+
+/**
+ * Created by Ahmed on 15-11-09.
+ */
+
+public class Message {
+    private DatagramPacket myReceivePacket;
+    private String data;
+    private InetAddress ipAddress;
+    private int port;
+
+    public Message(DatagramPacket myReceivePacket) {
+        this.myReceivePacket = myReceivePacket;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public InetAddress getIpAddress() {
+        return ipAddress;
+    }
+
+    public int getPort() {
+        return port;
+    }
+
+    public Message invoke() {
+        data = new String(myReceivePacket.getData(), 0, myReceivePacket.getLength());
+        System.out.println("RECEIVED Data: " + data);
+
+        ipAddress = myReceivePacket.getAddress();
+        System.out.println("RECEIVED Address: " + ipAddress);
+
+        port = myReceivePacket.getPort();
+        System.out.println("RECEIVED Port: " + port);
+        return this;
+    }
+}
--- Server/src/coen445/server/Server.java
+++ Server/src/coen445/server/Server.java
@@ -102,38 +102,4 @@ public class Server{
 
 
 
-    public static class Message {
-        private DatagramPacket myReceivePacket;
-        private String data;
-        private InetAddress ipAddress;
-        private int port;
-
-        public Message(DatagramPacket myReceivePacket) {
-            this.myReceivePacket = myReceivePacket;
-        }
-
-        public String getData() {
-            return data;
-        }
-
-        public InetAddress getIpAddress() {
-            return ipAddress;
-        }
-
-        public int getPort() {
-            return port;
-        }
-
-        public Message invoke() {
-            data = new String(myReceivePacket.getData(), 0, myReceivePacket.getLength());
-            System.out.println("RECEIVED Data: " + data);
-
-            ipAddress = myReceivePacket.getAddress();
-            System.out.println("RECEIVED Address: " + ipAddress);
-
-            port = myReceivePacket.getPort();
-            System.out.println("RECEIVED Port: " + port);
-            return this;
-        }
-    }
 }
